3db956955a0ddfb37ef99761dc953fe367ee6e71
Edgar Zambrana <edgarzambrana@Edgars-MacBook-Pro.local>
2023-06-02 12:36:53 -0400

    Fix typos in intro section component

--- src/app/experiences-page/experience-sections/experience-sections.module.ts
+++ src/app/experiences-page/experience-sections/experience-sections.module.ts
@@ -1,22 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IntoSectionComponent } from './into-section/into-section.component';
 import { ExperiencesSectionComponent } from './experiences-section/experiences-section.component';
 import { StayForTheRaceSectionComponent } from './stay-for-the-race-section/stay-for-the-race-section.component';
+import { IntroSectionComponent } from './intro-section/intro-section.component';
 
 
 
 @NgModule({
   declarations: [
-    IntoSectionComponent,
     ExperiencesSectionComponent,
-    StayForTheRaceSectionComponent
+    StayForTheRaceSectionComponent,
+    IntroSectionComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    IntoSectionComponent,
+    IntroSectionComponent,
     ExperiencesSectionComponent,
     StayForTheRaceSectionComponent
   ]
