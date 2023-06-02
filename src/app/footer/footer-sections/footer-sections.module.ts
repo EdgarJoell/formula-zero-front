153763ef26131412b02c6fd3bdfc3afda1a5d995
Edgar Zambrana <edgarzambrana@Edgars-MacBook-Pro.local>
2023-06-01 23:17:55 -0400

    Create footer sections. Update imports and exports

--- src/app/footer/footer-sections/footer-sections.module.ts
+++ src/app/footer/footer-sections/footer-sections.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LogoComponent } from './logo/logo.component';
+import { SocialsComponent } from './socials/socials.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LogoComponent,
+    SocialsComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    LogoComponent,
+    SocialsComponent
   ]
 })
 export class FooterSectionsModule { }
--- src/app/footer/footer.module.ts
+++ src/app/footer/footer.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterPageComponent } from './footer-page/footer-page.component';
+import { FooterSectionsModule } from './footer-sections/footer-sections.module';
 
 
 
@@ -9,7 +10,8 @@ import { FooterPageComponent } from './footer-page/footer-page.component';
     FooterPageComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FooterSectionsModule
   ],
   exports: [
     FooterPageComponent
