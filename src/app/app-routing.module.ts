a94bd6deaa37767e4b3c8dad97e49e9835c1c6e6
Edgar Zambrana <edgarzambrana@Edgars-MacBook-Pro.local>
2023-06-07 12:19:53 -0400

    Update routings to go to checkout screen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,7 +19,7 @@ const routes: Routes = [
       component: IndividualExperiencesComponent
    },
    {
-      path: 'cart',
+      path: 'experience-details/:id/cart',
       component: CartComponent
    }
 
