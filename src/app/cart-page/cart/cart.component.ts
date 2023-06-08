6ff7e8157d6cfba625a4e04b45bf9b701cf6b885
Edgar Zambrana <edgarzambrana@Edgars-MacBook-Pro.local>
2023-06-08 02:45:08 -0400

    Add more images. Fix cart adding prices problem

--- src/app/cart-page/cart/cart.component.ts
+++ src/app/cart-page/cart/cart.component.ts
@@ -10,13 +10,27 @@ import { CartService } from 'src/app/services/cart-api.service';
 })
 export class CartComponent {
   cart: any;
+  experiences: any;
 
   constructor(private route: ActivatedRoute, private experiencesService: ExperiencesService, private cartService: CartService) { }
+
+  totalPrice: number = 0;
+
+  calculateTotalPrice(): number {
+    let price: number = 0;
+    for(let i = 0; i < this.experiences.length; i++) {
+      price = price + this.experiences[i].price;
+      console.log(price);
+    }
+    return price;
+  }
 
   ngOnInit(): void {
     this.cart = this.cartService.getCartById(1).subscribe(p => {
       console.log(p);
       this.cart = p;
+      this.experiences = this.cart.experienceList;
     });
+    console.log(this.cart);
   }
 }
